e8d325176a7b17e54a748f95947a6c17cf8373ef
latamene <latamene@users.noreply.github.com>
2017-02-25 00:42:02 +0100

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.util.*;
+
+public class Test5
+{
+ 
+  static Scanner input = new Scanner(System.in);
+ 
+  public static void main(String[] args) {
+    int numNoms = getInt("Nombre d'entrées du tableau ?");
+    String[] noms = new String[numNoms];
+    for (int i = 0; i < noms.length; i++) {
+      noms[i] = getString("Entrée n°" + (i+1));
+    }
+    for (int i = 0; i < noms.length; ++i) {
+      System.out.println(noms[i]);
+    }
+  }
+ 
+  public static int getInt(String prompt) {
+    System.out.print(prompt + " ");
+    int entier = input.nextInt();
+    input.nextLine(); // Get rid of this line
+    // so that getString won't read it
+    return entier;
+  }
+ 
+  public static String getString(String prompt) {
+    System.out.print(prompt + " ");
+    return input.nextLine();
+  }
+}
+		
+		
+
+
